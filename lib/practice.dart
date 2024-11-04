9829e99b0f0c0195f7f1c8198e76fd87eebc7fe9
refatkhan <refatkhan897@gmail.com>
2024-11-04 21:47:52 +0600

    practice dart basic i/o,var/dynamic keyword, array,list

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,9 +1,17 @@
-import 'dart:io';
-
 void main(){
 
-  print('welcome to dart');
-  stdout.write('enter your name: ');
-  var name = stdin.readLineSync();
-  print("welcome to this site, $name");
+  var list = [10,20,40,230];
+  list.add(64);
+
+  print("$list");
+
+  // var list2 = [4,5,69];
+  // list2.addAll(list);
+  // print("$list2");
+
+  var list3 = [45,8,9,62];
+  list3.addAll(list);
+  print("$list3");
+
+
 }
