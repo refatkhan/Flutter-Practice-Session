98f1a866fe8a828e0f59cbd7696a33edb9617116
refatkhan <refatkhan897@gmail.com>
2024-11-08 21:10:13 +0600

    learn about list add remove insert and others things

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,17 +1,30 @@
-void main(){
-
-  var list = [10,20,40,230];
+void main() {
+  var list = [10, 20, 40, 230];
   list.add(64);
-
   print("$list");
 
   // var list2 = [4,5,69];
   // list2.addAll(list);
   // print("$list2");
 
-  var list3 = [45,8,9,62];
+  var list3 = [45, 8, 9, 62];
   list3.addAll(list);
   print("$list3");
-
-
+  // var name = [];
+  // name.add("abc");
+  // name.add("egg");
+  // print("$name");
+  // name.insert(0, "nais");
+  // print("$name");
+  // name.replaceRange(0, 2, ["dfe","rejk","fde"]);
+  // print("$name");
+//remove
+//   list3.removeLast();
+//   print("$list3");
+//   list3.removeAt(0);
+//   print("$list3");
+//   list3.removeRange(0, 2);
+//   print("$list3");
+//   print("${list3.length}");
+//   print("${list3.indexed}");
 }
